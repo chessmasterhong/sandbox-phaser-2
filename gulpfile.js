15f4a5a8118245746b491944f679d368452691ee
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-16 21:10:00 -0400

    Added gulp-webserver to Node dev-dependencies. Added watch and server gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@
 
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+    stylish = require('jshint-stylish'),
+    webserver = require('gulp-webserver');
 
 gulp.task('lint', function() {
-    return gulp.src([
-            './lib/game/**/*.js'
-        ])
+    return gulp.src('./lib/game/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter(stylish))
         .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./lib/game/**/*.js', ['lint']);
+});
+
+gulp.task('server', function() {
+    gulp.src('.')
+        .pipe(webserver({
+            host: '127.0.0.1',
+            port: 8080,
+            livereload: true
+        }));
 });
