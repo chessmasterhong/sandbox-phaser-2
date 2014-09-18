1eb65b48128dab4e78640a4a01c1aa4eedbdc58d
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-18 00:52:08 -0400

    Minor improvement to test for property existence.

--- test/specs/GameSpec.js
+++ test/specs/GameSpec.js
@@ -38,16 +38,16 @@
 
         describe('Input controls', function() {
             it('should have directional keybindings assigned', function() {
-                expect(Game.KEY.UP).to.exist;
+                expect(Game.KEY).to.have.property('UP');
                 expect(Game.KEY.UP.keyCode).to.equal(38); // Up arrow
 
-                expect(Game.KEY.DOWN).to.exist;
+                expect(Game.KEY).to.have.property('DOWN');
                 expect(Game.KEY.DOWN.keyCode).to.equal(40); // Down arrow
 
-                expect(Game.KEY.LEFT).to.exist;
+                expect(Game.KEY).to.have.property('LEFT');
                 expect(Game.KEY.LEFT.keyCode).to.equal(37); // Left arrow
 
-                expect(Game.KEY.RIGHT).to.exist;
+                expect(Game.KEY).to.have.property('RIGHT');
                 expect(Game.KEY.RIGHT.keyCode).to.equal(39); // Right arrow
             });
         });
